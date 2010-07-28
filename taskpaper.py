8a6978aa3084d2aae5e8ebf8a42abfc840cdf025
Björn B. Brandenburg <bbb@cs.unc.edu>
2010-07-28 12:32:35 -0400

    Rewrite library.
    Improvements:
    - shorter; less code duplication
    - unicode clean
    - simpler interface
    - flexible iteration interface
    - less chance to create an inconsistent data structure
      (e.g., have a note that ends with a ':')

--- taskpaper.py
+++ taskpaper.py
@@ -3,6 +3,9 @@ Created by Emil Erlandsson
 Modified by Matt Dawson
 Copyright (c) 2009 Matt Dawson
 
+Rewritten by Bjoern Brandenburg
+Copyright (c) 2010 Bjoern Brandenburg
+
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
@@ -20,31 +23,105 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 import types
 
-class Node(object):
-    """
-    A generic node on a TaskPaper object.
 
-    """
-    def __init__(self, line):
-        self.tabs = len(line) - len(line.lstrip('\t'))
-        #self.line = unicode(line, "utf-8").strip()
-        self.line = line.strip()
-        self.name = ""
-        self.parent = None
-        self.children = []
-        self.tags = []
-        self._parse()
+def extract_tags(line):
+    tags = {}
 
-    def set_parent(self, parent):
+    at  = None
+    arg = None
+
+    def add_tag(at, arg, last):
+        if arg:
+            tags[line[at+1:arg]]  = line[arg+1:last]
+        else:
+            tags[line[at+1:last]] = None
+
+    for i in xrange(len(line)):
+        if line[i] == '@':
+            if at is None:
+                # start of a new tag
+                at = i
+            elif i > 0 and line[i - 1] == ' ':
+                # missing closing ')' of previous argument
+                add_tag(at, arg, i)
+                arg = None
+                at = i
+        elif line[i] == ' ' and arg is None and not at is None:
+            # end of a tag without an argument
+            add_tag(at, arg, i)
+            at = None
+        elif line[i] == '(' and arg is None and not at is None:
+            # start of a tag argument
+            arg = i
+        elif line[i] == ')' and not arg is None:
+            # end of a tag with argument
+            add_tag(at, arg, i)
+            at  = None
+            arg = None
+    if not at is None:
+        # last tag ends at end-of-line
+        add_tag(at, arg, i)
+    return tags
+
+def indent_level(line):
+    level = 0
+    for i in xrange(len(line)):
+        if line[i] != '\t':
+            break
+        level += 1
+    return level
+
+
+class TaskItem(object):
+    """
+    An entry in a TaskPaper file. Corresponds to one line.
+    """
+
+    def __init__(self, txt="", tags={}, items=None, parent=None):
+        self.txt = txt.strip()
         self.parent = parent
+        if not items:
+            items = []
+        self.items = items
+        self.tags = tags
+        assert parent != self
+        if parent:
+            parent.add_item(self)
 
-    def add_child(self, child):
-        self.children.append(child)
+    @staticmethod
+    def parse(line, parent=None):
+        tag_start = line.find('@')
+        if tag_start != -1:
+            tags = extract_tags(line[tag_start:])
+            return TaskItem(line[:tag_start], tags=tags, parent=parent)
+        else:
+            return TaskItem(line, parent=parent)
+
+    def add_item(self, item):
+        self.items.append(item)
+
+    def is_task(self):
+        return self.txt.startswith('- ')
+
+    def is_project(self):
+        return self.txt.endswith(':') and not self.is_task()
+
+    def is_note(self):
+        return not (self.is_task() or self.is_project())
 
     def delete(self):
-        "Remove node from parent's children list."
+        "Remove node from parent's item list."
         if self.parent:
-            self.parent.children.remove(self)
+            self.parent.items.remove(self)
+
+    def level(self):
+        "Count how far this node is removed from the top level"
+        count = -1
+        cur = self
+        while cur.parent:
+            count += 1
+            cur = cur.parent
+        return count
 
     def add_tag(self, tag, value=None):
         self.tags[tag] = value
@@ -56,102 +133,70 @@ class Node(object):
         else:
             return False
 
-    def __str__(self):
-        return "%(tabs)s%(line)s" % { 'tabs': (self.tabs*'\t'), 'line': self.line }
-
-
-class ProjectNode(Node):
-    """
-    A project node on a TaskPaper object.
-
-    """
-    def _parse(self):
-        line = self.line.strip()
-        tokens = line.split("@")
-        self.name = tokens[0][:-1].strip()
-        self.tags = tokens[1:]
-
-class TaskNode(Node):
-    """
-    A task node on a TaskPaper object.
-
-    """
-    def _parse(self):
-        line = self.line.strip()
-        tokens = line.split("@")
-        self.name = tokens[0][2:].strip()
-        self.tags = self._parse_tags(tokens[1:])
-
-    def _parse_tags(self, tags):
-        """
-        >>> t = TaskNode("\t\t- Task @tag @tagWithValue(100)")
-        >>> len(t.tags.keys())
-        2
-        >>> print t.tags['tag']
-        None
-        >>> print t.tags['tagWithValue']
-        100
-        """
-        tag_dict = {}
-        for tag in tags:
-            name, value = self._parse_tag(tag)
-            tag_dict[name] = value
-        return tag_dict
-
-    def _parse_tag(self, tag):
-        tag = tag.strip()
-        s = tag.split("(")
-        return s[0], s[1][:-1] if len(s) == 2 else None
+    def format(self, with_tabs=True):
+        tag_txt  = " ".join(['@%s' % x if self.tags[x] is None else "%s(%s)" % (x, self.tags[x])
+                             for x in self.tags])
+        tabs_txt = '\t' * self.level() if with_tabs else ''
+        return "%s%s%s%s" % (tabs_txt, self.txt, ' ' if tag_txt else '', tag_txt)
 
     def __str__(self):
-        """
-        >>> t = TaskNode("\t\t- Task @tag @tagWithValue(100)")
-        >>> t.tags['tag'] = "value"
-        >>> print t
-        - Task @tag(value) @tagWithValue(100)
-        """
-        tags = ['@%s(%s)' % (t[0], t[1]) if t[1] else "@%s" % t[0] for t in self.tags.items()]
-        return "%(tabs)s- %(name)s %(tags)s" % { 
-                'tabs': (self.tabs * '\t'), 'name': self.name, 'tags': ' '.join(tags) }
+        return self.format(False)
 
-class NoteNode(Node):
-    """
-    A note node on a TaskPaper object.
-
-    """
-    def _parse(self):
-        line = self.line.strip()
-        tokens = line.split("@")
-        self.name = tokens[0].strip()
-        self.tags = tokens[1:]
 
 class TaskPaper(object):
     """
     A wrapper class for TaskPaper files.
-
-
     """
-    def __init__(self, url):
-        self.url = url 
-        self.filename = url.split("/")[-1]
-        self.children= []
 
-    def add_child(self, node):
-        """
-        Adds a no-parent node to a TaskPaper object.
+    def __init__(self):
+        self.items  = []
+        self.parent = None
 
-        """
-        self.children.append(node)
+    @staticmethod
+    def parse(lines):
+        tp = TaskPaper()
+        last_level = -1
+        last_item  = tp
+        for line in lines:
+            level = indent_level(line)
+            if level == last_level:
+                # sibling
+                last_item = TaskItem.parse(line, last_item.parent)
+            elif level > last_item:
+                # sub-item
+                last_item = TaskItem.parse(line, last_item)
+            else:
+                # go back up
+                last_item = TaskItem.parse(line, tp.last_item(level - 1))
+            last_level = level
+        return tp
+
+    def add_item(self, item):
+        self.items.append(item)
+
+    def level(self):
+        return 0
+
+    def last_item(self, level=0):
+        item = self
+        while level >= 0:
+            level -= 1
+            if item.items:
+                item = item.items[-1]
+            else:
+                break
+        return item
 
     def select(self, predicate):
-        "Iterate over nodes, yield only those that match the predicate."
+        "Iterate over all items, yield only those that match the predicate."
         to_visit = []
-        to_visit.extend(self.children)
+        to_visit.extend(reversed(self.items))
         while to_visit:
-            node = to_visit.pop()
-            to_visit.extend(node.children)
-            if predicate(node):
-                yield node
+            item = to_visit.pop()
+            to_visit.extend(reversed(item.items))
+            assert not item in item.items
+            if predicate(item):
+                yield item
 
     def __getitem__(self, key):
         # does it look like a string?
@@ -160,107 +205,14 @@ class TaskPaper(object):
             return self.select(lambda nd: key in nd.tags)
         else:
             # assume it's an index
-            return self.children[key]
+            return self.items[key]
 
-    def filter_by_tag(self, tag_name):
-        """
-        >>> tp = parse_taskpaper('test.taskpaper')
-        >>> tasks = tp.filter_by_tag("tag")
-        >>> len(tasks)
-        3
-        >>> len(tp.filter_by_tag('tagWithValue'))
-        1
-        """
-        return list(self.select(lambda nd: isinstance(nd, TaskNode) \
-                                    and tag_name in nd.tags))
+    def __iter__(self):
+        "Iterate over all items."
+        return self.select(lambda _: True)
+
+    def format(self):
+        return "\n".join([nd.format(True) for nd in self])
 
     def __str__(self):
-        def to_string(nodes):
-            output = [str(node) + '\n' + to_string(node.children) for node in nodes]
-            return ''.join(output)
-        tp_string = to_string(self.children)
-        return tp_string
-
-def parse_taskpaper_from_string(string):
-    import StringIO
-    handle = StringIO.StringIO()
-    handle.write(string)
-    handle.seek(0)
-
-    return parse_taskpaper_from_file(handle)
-
-
-def parse_taskpaper(url):
-    """
-    Parse a TaskPaper file and return a corresponding TaskPaper object.
-
-    """
-    handle = file(url)
-
-    return parse_taskpaper_from_file(handle, url)
-
-def parse_taskpaper_from_file(handle, url = ''):
-    taskpaper = TaskPaper(url)
-    last = None
-    for line in handle.readlines():
-
-        # Determine what kind of node is on the current line.
-        if line.strip().startswith("- "):
-            node = TaskNode(line)
-        elif line.strip().endswith(":"):
-            node = ProjectNode(line)
-        elif line.strip() != '':
-            node = NoteNode(line)
-        else:
-            # The line is blank.
-            node = None
-
-        # If the line is blank, ignore it.
-        if node is not None:
-            # If this is not the beginning of the document and the line is
-            # tabbed, proceed.
-            if (last is not None) and (node.tabs != 0):
-                if (node.tabs > last.tabs) or (type(node) == NoteNode):
-                    last.add_child(node)
-                    node.set_parent(last)
-                else:
-                    if node.tabs < last.tabs:
-                        # Climb the document until we reach the closest sibling.
-                        while node.tabs < last.tabs:
-                            last = last.parent
-                    last.parent.add_child(node)
-                    node.set_parent(last.parent)
-            # If this is the beginning of the document or the line is not tabbed,
-            # add the node as a child of the taskpaper document. 
-            else:
-                taskpaper.add_child(node)
-                node.set_parent(taskpaper)
-
-            # Unless the current node is a note, assign it to last for the next
-            # iteration.
-            if type(node) != NoteNode:
-                last = node
-
-    return taskpaper
-
-def print_tree(node):
-    """
-    For any Taskpaper object, print it and a formatted tree of its descendants.
-
-    """
-    try:
-        print node.filename
-    except AttributeError:
-        print (node.tabs * '\t') + node.line
-    for child in node.children:
-        print_tree(child)
-
-def write_tree(node, url):
-    f = open(url, 'wb')
-    def to_string(nodes):
-        output = [(node.tabs * '\t') + node.line + '\n' + to_string(node.children) for node in nodes]
-        return ''.join(output)
-    tp_string = to_string(node.children)
-    f.write(tp_string)
-    f.close()
-
+        return self.format()
